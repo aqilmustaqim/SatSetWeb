07326f3dd22a07741e4ca4d700a8732d137b725a
alvin <zeronep960@ittelkom-sby.ac.id>
2022-06-08 11:06:12 +0700

    update hover

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,7 +1,7 @@
 <?= $this->extend('templates/index'); ?>
 <?= $this->section('content'); ?>
 <section class="vh-100">
-    <div class="container py-5 h-100">
+    <div class="container py-5">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                 <div class="card shadow-2-strong" style="border-radius: 1rem;">
