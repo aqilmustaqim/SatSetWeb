73bc8408ffeb9dc31cf5e4e5e9492b1ae44ac0c6
Fiqri Baihaqi Hermawanto <fiqto14@student.ittelkom-sby.ac.id>
2022-06-15 12:11:43 +0700

    Edit kelola portfolio

--- app/Views/admin/kelola_portfolio.php
+++ app/Views/admin/kelola_portfolio.php
@@ -1,18 +1,13 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php $this->extend('templates/admin'); ?>
+<?php $this->section('content'); ?>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Kelola Portoflio</title>
-  </head>
-  <body>
+
+    <section class="home">
+    <h1 class="text py-3">Kelola Portfolio</h1>
     <div class="container">
-        <h1 class="text-center py-3">Kelola Portfolio</h1>
         <table class="table">
             <button type="button" class="btn btn-success mb-3">Create New Portfolio</button>
         <thead>
@@ -58,17 +53,9 @@
         </tbody>
         </table>
     </div>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
+    </section>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+$<?php $this->endSection(); ?>
