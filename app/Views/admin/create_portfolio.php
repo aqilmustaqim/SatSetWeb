88e539e73f9e958159a657fc5e7242872feeb904
Fiqri Baihaqi Hermawanto <fiqto14@student.ittelkom-sby.ac.id>
2022-06-23 15:00:58 +0700

    popup save

--- app/Views/admin/create_portfolio.php
+++ app/Views/admin/create_portfolio.php
@@ -35,8 +35,26 @@
         <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Masukkan Link Website">
         </div>
 
-        <a class="btn btn-sm btn-primary" href="<?= base_url('Home/porto'); ?>" role="button">Save</a>
-                    
+        <a class="btn btn-primary" href="<?= base_url('Home/porto'); ?>" role="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Save</a>
+        
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create New Portfolio</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Apakah kamu yakin membuat portfolio baru ini?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <a class="btn btn-primary" href="<?= base_url('Home/porto'); ?>" role="button">Save change</a>
+            </div>
+            </div>
+        </div>
+        </div>
+
     </div>
     </section>
 
