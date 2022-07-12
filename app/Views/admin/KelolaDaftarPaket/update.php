348f774a300325022ab3d552c3c3331aa2eac2ee
alvin <zeronep960@ittelkom-sby.ac.id>
2022-07-12 20:59:08 +0700

    add folder and file view kelola daftar paket

---
+++ app/Views/admin/KelolaDaftarPaket/update.php
@@ -0,0 +1,6 @@
+<?php $this->extend('templates/admin'); ?>
+<?php $this->section('content'); ?>
+<section class="home">
+	<h1 class="text text-center">Update Daftar Menu</h1>
+</section>
+<?php $this->endSection(); ?>
